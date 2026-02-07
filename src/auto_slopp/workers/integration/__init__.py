0f073473d548cbf6f36ea7018c603ba1967e25d7
Melvin Klein <70433111+MelvinKl@users.noreply.github.com>
2026-02-07 23:38:45 +0100

    Delete src/auto_slopp/workers/integration directory

--- src/auto_slopp/workers/integration/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Integration workers package.
-
-Contains workers for external system integrations.
-"""
-
-from .beads_worker import BeadsTaskWorker
-from .openagent_worker import OpenAgentWorker
-
-__all__ = ["BeadsTaskWorker", "OpenAgentWorker"]
