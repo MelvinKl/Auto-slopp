9dfc8388e6241f7fa6617ae689d7ba2f56820316
Test User <test@test.com>
2026-02-12 22:47:09 +0000

    Add setup.cfg, setup.py and my_test_package.egg-info

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_test_package",
+    version="0.1.0",
+    description="My test package",
+    author="Test Author",
+    author_email="test@example.com",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
