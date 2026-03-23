7afbbfa39fc4c0313b6d8893f09eab85e0b1b6dd
Melvin Klein <70433111+MelvinKl@users.noreply.github.com>
2026-03-23 12:07:54 +0000

    test: verify pre-commit hook formats and stages files

---
+++ tests/test_formatting.py
@@ -0,0 +1,17 @@
+"""Test file for formatting hooks."""
+
+import os
+import sys
+
+
+def badly_formatted(x, y):
+    """A badly formatted function."""
+    return x + y
+
+
+class TestClass:
+    def __init__(self):
+        self.value = 1
+
+    def method(self):
+        pass
